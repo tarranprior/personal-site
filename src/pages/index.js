02b7dad15978e2f29e9229301e196c6cfe180f7b
Tarran Prior <Tarranprior@protonmail.com>
2022-05-28 12:06:45 +0100

    🔧 Update personal-site/: Initial markup, add Layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,23 @@
-import React from "react"
+import React from "react";
+import Layout from '../components/Layout';
 
 export default function Home() {
-  return <div>Hello world!</div>
-}
+  return (
+    <div class="container">
+      <Layout>
+        <main>
+          <h1>Hey, I'm Tarran! 👋</h1>
+          <p>I'm passionate about open source, solving problems and collaborative innovation.<br/>I also work with data a lot.</p>
+          <h3>Currently:</h3>
+          <ul>
+            <li>👨‍💻 I'm an aspiring security engineer living in beautiful <b>Wiltshire, UK</b></li>
+            <li>🏢 installing <b>secure and scalable IT infrastructure</b> for local businesses,</li>
+            <li>🛠️ building <b>community-driven bots</b> @ <b><a href="https://discord.com/" target="_blank" rel="noreferrer">Discord</a></b> and some <b>OSINT tools</b>,</li>
+            <li>💾 <b>preserving files</b> and discussing data with the good people @ <b><a href="https://old.reddit.com/r/DataHoarder" target="_blank" rel="noreferrer">r/DataHoarder</a></b>,</li>
+            <li>🌱 learning <b>Go</b>, <b>Reverse Engineering</b> and <b>React Native Development</b>.</li>
+          </ul>
+        </main>
+      </Layout>
+    </div>
+  )
+};
